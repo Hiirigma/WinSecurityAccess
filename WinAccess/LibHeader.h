4117669fb64ebd954f47321d4de9047d33d009a6
Hiirigma <hariton1599@gmail.com>
2020-03-07 18:04:54 +0300

    Add new functions (users and group). Bug :: can't get user group

--- WinAccess/LibHeader.h
+++ WinAccess/LibHeader.h
@@ -1,2 +1,65 @@
-#include <iostream>
+#include <stdio.h>
 #include <Windows.h>
+#include <locale.h>
+// For outUsers and netusersenum
+#define NET_API_STATUS DWORD
+#define NERR_Success 0
+
+// API Prototype for Netapi32.dll!NetUserEnum
+typedef NET_API_STATUS(__stdcall *PROC_NetUserEnum)(
+	LPCWSTR servername,
+	DWORD level,
+	DWORD filter,
+	LPBYTE* bufptr,
+	DWORD prefmaxlen,
+	LPDWORD entriesread,
+	LPDWORD totalentries,
+	LPDWORD resume_handle
+	);
+
+
+// API Prototype for Netapi32.dll!NetLocalGroupEnum
+typedef NET_API_STATUS(__stdcall *PROC_NetLocalGroupEnum)(
+	LPCWSTR servername,
+	DWORD level,
+	LPBYTE* bufptr,
+	DWORD prefmaxlen,
+	LPDWORD entriesread,
+	LPDWORD totalentries,
+	PDWORD_PTR resumehandle
+	);
+
+// API Prototype for Netapi32.dll!NetUserGetGroups
+typedef NET_API_STATUS(__stdcall *PROC_NetUserGetGroups)(
+	LPCWSTR servername,
+	LPCWSTR username,
+	DWORD level,
+	LPBYTE* bufptr,
+	DWORD prefmaxlen,
+	LPDWORD entriesread,
+	LPDWORD totalentries
+	);
+
+
+
+typedef struct _USER_INFO_1 {
+	LPWSTR   usri1_name;
+	LPWSTR   usri1_password;
+	DWORD    usri1_password_age;
+	DWORD    usri1_priv;
+	LPWSTR   usri1_home_dir;
+	LPWSTR   usri1_comment;
+	DWORD    usri1_flags;
+	LPWSTR   usri1_script_path;
+}USER_INFO_1, *PUSER_INFO_1, *LPUSER_INFO_1;
+
+typedef struct _GROUP_USERS_INFO_1 {
+	LPWSTR  grui1_name;
+	DWORD   grui1_attributes;
+} GROUP_USERS_INFO_1, *PGROUP_USERS_INFO_1, *LPGROUP_USERS_INFO_1;
+
+
+typedef struct _LOCALGROUP_INFO_0 {
+	LPWSTR   lgrpi0_name;
+}LOCALGROUP_INFO_0, *PLOCALGROUP_INFO_0, *LPLOCALGROUP_INFO_0;
+
--- WinAccess/WinAccess.cpp
+++ WinAccess/WinAccess.cpp
@@ -8,8 +8,8 @@
 //		What to do ::
 
 //		View Registåred ::
-// - Users 
-// - Groups
+// V - Users	
+// V - Groups
 // - SID 
 // - Privilleges
 //		Must change ::
@@ -18,17 +18,112 @@
 // - Privilleges
 
 #include "LibHeader.h"
-#pragma comment(lib, "Advapi32.lib")
-#pragma comment(lib, "netapi32.lib")
+
+void outUsers(HMODULE hModule) {
+	// API Prototype for Netapi32.dll!NetUserEnum
+	NET_API_STATUS status;
+	DWORD entries;
+	DWORD guent;
+	DWORD entRead;
+	DWORD guentread;
+
+	DWORD uPrefLen = 20;
+	DWORD ugPrefLen = 20;
+	DWORD guPrefLen = 50;
+	_USER_INFO_1 * pUserBuf = new USER_INFO_1[20];
+	_LOCALGROUP_INFO_0 * pGroupBuf = new LOCALGROUP_INFO_0[255];
+	_GROUP_USERS_INFO_1 * puGroupBuf = new GROUP_USERS_INFO_1[50];
+
+	NET_API_STATUS usrRes;
+	NET_API_STATUS grRes;
+	NET_API_STATUS gruRes;
+
+	if (hModule != NULL)
+	{
+		PROC_NetUserEnum _NetUserEnum = (PROC_NetUserEnum)GetProcAddress(hModule, "NetUserEnum");
+		PROC_NetLocalGroupEnum _NetLocalGroupEnum = (PROC_NetLocalGroupEnum)GetProcAddress(hModule, "NetLocalGroupEnum");
+		PROC_NetUserGetGroups _NetUserGetGroups = (PROC_NetUserGetGroups)GetProcAddress(hModule, "NetUserGetGroups");
+
+
+		if (_NetUserEnum != NULL && _NetLocalGroupEnum != NULL)
+		{
+			// TODO: Set Parameter Values
+			usrRes = _NetUserEnum(
+				NULL,
+				1, // this attribute to change structure fields 
+				0,
+				(LPBYTE*)&pUserBuf,
+				uPrefLen,
+				/*out*/&entRead,
+				/*out*/&entries,
+				/*out resumehandle*/ NULL
+			);
+
+			
+			if (usrRes == NERR_Success)
+			{
+				printf("\tList of users in system :: \n");
+				for (int i = 0;i!=entries; i++) {
+					if (pUserBuf[i].usri1_name == NULL) break;
+					gruRes = _NetUserGetGroups(
+						NULL,
+						pUserBuf[i].usri1_name,
+						1,
+						(LPBYTE*)&puGroupBuf,
+						guPrefLen,
+						&guent,
+						&guentread
+					);
+					printf("%S\n", pUserBuf[i].usri1_name);
+					for (int j = 0; j < guentread; j++) {
+						printf("%S; ", puGroupBuf[j].grui1_name);
+					}
+					printf("\n\n");
+				}
+			}
+			delete[] pUserBuf;
+
+			entRead = 0;
+			entries = 0;
+			grRes = _NetLocalGroupEnum(
+				NULL,
+				0,
+				(LPBYTE*)&pGroupBuf,
+				-1,
+				/*out*/&entRead,
+				/*out*/&entries,
+				NULL
+			);
+
+			if (grRes == NERR_Success) {
+				printf("\tList of groups in system :: \n");
+				for (int i = 0; i != entries; i++) {
+					if (pGroupBuf[i].lgrpi0_name == NULL) break;
+					printf("%S\n", pGroupBuf[i].lgrpi0_name);
+				}
+			}
+
+		}
+
+	}
+
+}
+
 
 
 
 int main(void)
 {
-	HINSTANCE hinstLib;
-	BOOL fFreeResult, fRunTimeLinkSuccess = FALSE;
-	hinstLib = LoadLibrary(TEXT("MyPuts.dll"));
+	setlocale(LC_ALL, "Russian");
+	HMODULE hModule = LoadLibrary(("Netapi32.dll"));
+	if (hModule == NULL) {
+		printf("Error in loading library\n");
+		exit(-1);
+	}
 
+	outUsers(hModule);
+	FreeLibrary(hModule);
+	system("pause");
     return 0;
 }
 
