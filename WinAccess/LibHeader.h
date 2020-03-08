b9b60a27ab02c6811f7eba4137a46524feb2da7e
Hiirigma <hariton1599@gmail.com>
2020-03-08 14:28:30 +0300

    Add sid information output and heap functions via kernel.dll

--- WinAccess/LibHeader.h
+++ WinAccess/LibHeader.h
@@ -4,7 +4,8 @@
 // For outUsers and netusersenum
 #define NET_API_STATUS DWORD
 #define NERR_Success 0
-
+#define LG_INCLUDE_INDIRECT 1
+#define MAX_PREFERRED_LENGTH -1
 // API Prototype for Netapi32.dll!NetUserEnum
 typedef NET_API_STATUS(__stdcall *PROC_NetUserEnum)(
 	LPCWSTR servername,
@@ -29,17 +30,53 @@ typedef NET_API_STATUS(__stdcall *PROC_NetLocalGroupEnum)(
 	PDWORD_PTR resumehandle
 	);
 
-// API Prototype for Netapi32.dll!NetUserGetGroups
-typedef NET_API_STATUS(__stdcall *PROC_NetUserGetGroups)(
+// API Prototype for Netapi32.dll!NetUserGetLocalGroups
+typedef NET_API_STATUS(__stdcall *PROC_NetUserGetLocalGroups)(
 	LPCWSTR servername,
 	LPCWSTR username,
 	DWORD level,
+	DWORD flags,
 	LPBYTE* bufptr,
 	DWORD prefmaxlen,
 	LPDWORD entriesread,
 	LPDWORD totalentries
 	);
 
+// API Prototype for Advapi32.dll!LookupAccountNameW
+typedef BOOL(__stdcall *PROC_LookupAccountNameW)(
+	LPCTSTR lpSystemName,
+	LPCTSTR lpAccountName,
+	PSID Sid,
+	LPDWORD cbSid,
+	LPTSTR ReferencedDomainName,
+	LPDWORD cchReferencedDomainName,
+	PSID_NAME_USE peUse
+	);
+
+
+// API Prototype for Advapi32.dll!ConvertSidToStringSidA
+typedef BOOL(__stdcall *PROC_ConvertSidToStringSidA)(
+	PSID Sid,
+	LPTSTR* StringSid
+	);
+
+// API Prototype for Kernel32.dll!HeapAlloc
+typedef LPVOID(__stdcall *PROC_HeapAlloc)(
+	HANDLE hHeap,
+	DWORD dwFlags,
+	SIZE_T dwBytes
+	);
+
+// API Prototype for Kernel32.dll!HeapFree
+typedef BOOL(__stdcall *PROC_HeapFree)(
+	HANDLE hHeap,
+	DWORD dwFlags,
+	LPVOID lpMem
+	);
+
+// API Prototype for Kernel32.dll!GetProcessHeap
+typedef HANDLE(__stdcall *PROC_GetProcessHeap)(
+	);
 
 
 typedef struct _USER_INFO_1 {
@@ -59,7 +96,11 @@ typedef struct _GROUP_USERS_INFO_1 {
 } GROUP_USERS_INFO_1, *PGROUP_USERS_INFO_1, *LPGROUP_USERS_INFO_1;
 
 
+typedef struct _LOCALGROUP_USERS_INFO_0 {
+	LPWSTR lgrui0_name;
+} LOCALGROUP_USERS_INFO_0, *PLOCALGROUP_USERS_INFO_0, *LPLOCALGROUP_USERS_INFO_0;
+
+
 typedef struct _LOCALGROUP_INFO_0 {
 	LPWSTR   lgrpi0_name;
 }LOCALGROUP_INFO_0, *PLOCALGROUP_INFO_0, *LPLOCALGROUP_INFO_0;
-
--- WinAccess/WinAccess.cpp
+++ WinAccess/WinAccess.cpp
@@ -19,31 +19,87 @@
 
 #include "LibHeader.h"
 
-void outUsers(HMODULE hModule) {
+int getSid(LPWSTR username, HMODULE advHandle, HMODULE kernHandle) {
+	LPTSTR sid_str;
+	LPWSTR domain = NULL;
+	DWORD size = 0, dom_size = 0;
+	SID_NAME_USE peUse;
+	PSID  psid = NULL;
+	UINT ret_val = ERROR_FUNCTION_FAILED;
+
+	PROC_ConvertSidToStringSidA _ConvertSidToStringSidA = (PROC_ConvertSidToStringSidA)GetProcAddress(advHandle, "ConvertSidToStringSidA");
+	PROC_LookupAccountNameW _LookupAccountNameW = (PROC_LookupAccountNameW)GetProcAddress(advHandle, "LookupAccountNameW");
+	PROC_HeapAlloc _HeapAlloc = (PROC_HeapAlloc)GetProcAddress(kernHandle, "HeapAlloc");
+	PROC_GetProcessHeap _GetProcessHeap = (PROC_GetProcessHeap)GetProcAddress(kernHandle, "GetProcessHeap");
+	PROC_HeapFree _HeapFree = (PROC_HeapFree)GetProcAddress(kernHandle, "HeapFree");
+
+	// TODO: Set Parameter Values
+	_LookupAccountNameW(NULL, (LPCSTR)username, NULL, &size, NULL, &dom_size, &peUse);
+
+	psid = (PSID)_HeapAlloc(GetProcessHeap(),
+		HEAP_ZERO_MEMORY,
+		size);
+
+	domain = (LPWSTR)_HeapAlloc(GetProcessHeap(),
+		HEAP_ZERO_MEMORY,
+		dom_size * sizeof(WCHAR));
+
+
+	if (!psid || !domain)
+	{
+		return (ERROR_OUTOFMEMORY);
+		
+	}
+
+	ret_val = _LookupAccountNameW(NULL, (LPCSTR)username, psid, &size, (LPTSTR)domain, &dom_size, &peUse);
+	if (!ret_val) return ret_val;
+
+	ret_val = _ConvertSidToStringSidA(psid, &sid_str);
+	if (!ret_val) return ret_val;
+
+	printf("SID :: %s\n", sid_str);
+
+	_HeapFree(_GetProcessHeap(),
+		0,
+		psid);
+
+	_HeapFree(_GetProcessHeap(),
+		0,
+		domain);
+
+	return 0;
+}
+
+
+void outUsers() {
 	// API Prototype for Netapi32.dll!NetUserEnum
+	HMODULE netHandle = LoadLibrary(("Netapi32.dll"));
+	HMODULE advHandle = LoadLibrary(("Advapi32.dll"));
+	HMODULE kernHandle = LoadLibrary(("Kernel32.dll"));
+
+	if (netHandle == NULL || advHandle == NULL || kernHandle == NULL) {
+		printf("Error in loading library\n");
+		exit(-1);
+	}
+
 	NET_API_STATUS status;
 	DWORD entries;
 	DWORD guent;
 	DWORD entRead;
 	DWORD guentread;
-
-	DWORD uPrefLen = 20;
-	DWORD ugPrefLen = 20;
-	DWORD guPrefLen = 50;
 	_USER_INFO_1 * pUserBuf = new USER_INFO_1[20];
-	_LOCALGROUP_INFO_0 * pGroupBuf = new LOCALGROUP_INFO_0[255];
-	_GROUP_USERS_INFO_1 * puGroupBuf = new GROUP_USERS_INFO_1[50];
-
+	_LOCALGROUP_INFO_0  * pGroupBuf = new _LOCALGROUP_INFO_0[255];
+	_LOCALGROUP_USERS_INFO_0 * puGroupBuf = new LOCALGROUP_USERS_INFO_0[50];
+	PSID Sid;
 	NET_API_STATUS usrRes;
 	NET_API_STATUS grRes;
 	NET_API_STATUS gruRes;
 
-	if (hModule != NULL)
+	if (netHandle != NULL)
 	{
-		PROC_NetUserEnum _NetUserEnum = (PROC_NetUserEnum)GetProcAddress(hModule, "NetUserEnum");
-		PROC_NetLocalGroupEnum _NetLocalGroupEnum = (PROC_NetLocalGroupEnum)GetProcAddress(hModule, "NetLocalGroupEnum");
-		PROC_NetUserGetGroups _NetUserGetGroups = (PROC_NetUserGetGroups)GetProcAddress(hModule, "NetUserGetGroups");
-
+		PROC_NetUserEnum _NetUserEnum = (PROC_NetUserEnum)GetProcAddress(netHandle, "NetUserEnum");
+		PROC_NetLocalGroupEnum _NetLocalGroupEnum = (PROC_NetLocalGroupEnum)GetProcAddress(netHandle, "NetLocalGroupEnum");
+		PROC_NetUserGetLocalGroups _NetUserGetLocalGroups = (PROC_NetUserGetLocalGroups)GetProcAddress(netHandle, "NetUserGetLocalGroups");
 
 		if (_NetUserEnum != NULL && _NetLocalGroupEnum != NULL)
 		{
@@ -53,7 +109,7 @@ void outUsers(HMODULE hModule) {
 				1, // this attribute to change structure fields 
 				0,
 				(LPBYTE*)&pUserBuf,
-				uPrefLen,
+				MAX_PREFERRED_LENGTH,
 				/*out*/&entRead,
 				/*out*/&entries,
 				/*out resumehandle*/ NULL
@@ -65,31 +121,41 @@ void outUsers(HMODULE hModule) {
 				printf("\tList of users in system :: \n");
 				for (int i = 0;i!=entries; i++) {
 					if (pUserBuf[i].usri1_name == NULL) break;
-					gruRes = _NetUserGetGroups(
+					gruRes = _NetUserGetLocalGroups(
 						NULL,
 						pUserBuf[i].usri1_name,
-						1,
+						0,
+						LG_INCLUDE_INDIRECT,
 						(LPBYTE*)&puGroupBuf,
-						guPrefLen,
+						MAX_PREFERRED_LENGTH,
 						&guent,
 						&guentread
 					);
+
+
+
 					printf("%S\n", pUserBuf[i].usri1_name);
+					if (guentread == 0)
+						printf("None;");
 					for (int j = 0; j < guentread; j++) {
-						printf("%S; ", puGroupBuf[j].grui1_name);
+						if (puGroupBuf[j].lgrui0_name != NULL)
+							printf("%S; ", puGroupBuf[j].lgrui0_name);
 					}
-					printf("\n\n");
+
+					printf("\n");
+					usrRes = getSid(pUserBuf[i].usri1_name, advHandle,kernHandle);
+					if (usrRes)
+						printf("Get sid error with code :: %d", usrRes);
+					printf("\n");
 				}
 			}
-			delete[] pUserBuf;
-
 			entRead = 0;
 			entries = 0;
 			grRes = _NetLocalGroupEnum(
 				NULL,
 				0,
 				(LPBYTE*)&pGroupBuf,
-				-1,
+				MAX_PREFERRED_LENGTH,
 				/*out*/&entRead,
 				/*out*/&entries,
 				NULL
@@ -106,24 +172,19 @@ void outUsers(HMODULE hModule) {
 		}
 
 	}
-
+	delete[] pUserBuf;
+	delete[] puGroupBuf;
+	delete[] pGroupBuf;
+	FreeLibrary(netHandle);
+	FreeLibrary(advHandle);
+	FreeLibrary(kernHandle);
 }
-
-
 
 
 int main(void)
 {
 	setlocale(LC_ALL, "Russian");
-	HMODULE hModule = LoadLibrary(("Netapi32.dll"));
-	if (hModule == NULL) {
-		printf("Error in loading library\n");
-		exit(-1);
-	}
-
-	outUsers(hModule);
-	FreeLibrary(hModule);
+	outUsers();
 	system("pause");
     return 0;
 }
-
